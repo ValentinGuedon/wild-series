199b71a83283604381aeb72fed1630bc132f6784
ValentinGuedon <guedonvalentin@gmail.com>
2023-05-22 16:45:53 +0200

    added routes quest 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,18 +1,27 @@
 <?php
-// src/Controller/ProgramController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'program_index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
          ]);
+    }
+
+    #[Route('/{id}', name: 'program_show', requirements: ['id'=>'\d+'], methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
     }
 }
