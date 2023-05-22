502525c96c3914858c4373970ae7e180bca5093c
ValentinGuedon <guedonvalentin@gmail.com>
2023-05-22 20:56:32 +0200

    quest7 fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 1; $i <= 50; $i++) {  
+            $category = new Category();  
+            $category->setName('Nom de catégorie ' . $i);  
+            $manager->persist($category);  
+        }  
+        $manager->flush();
+    }
+}
