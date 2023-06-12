ad7124907fe842142d71daea414813b026d9951c
ValentinGuedon <guedonvalentin@gmail.com>
2023-06-12 11:11:46 +0200

    Quest18

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -2,26 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Actor;
+use App\Repository\ActorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/actor', name: 'app_actor')]
+#[Route('/actor', name: 'actor')]
 class ActorController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ActorRepository $actorRepository): Response
     {
+        $actors = $actorRepository->findAll();
+
         return $this->render('actor/index.html.twig', [
-            'controller_name' => 'ActorController',
+            'actors' => $actors
         ]);
     }
 
-    #[Route('/{id}', name: '_show')]#
-    public function show(): Response
+    #[Route('/{id<^[0-9]+$>}', name: '_show')]#
+    public function show(Actor $actor): Response
     {
         return $this->render('actor/show.html.twig', [
-            'controller_name' => 'ActorController',
+            'actor' => $actor
         ]);
     }
 }
